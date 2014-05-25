66a358068e228d77d8d580f5019b2fb82c945f2c
Roger Sliva <roger.sliva@mesaaz.gov>
2014-05-24 18:48:48 -0700

    initial draft of assignment 2 R functions to cache matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -4,12 +4,40 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(b) m <<- b
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        # found cached inverted matrix - use it
+        message("returning cached value")
+        return(m)
+    } else {
+        # Get inverse of matrix
+        # Excepton handling - see http://www1.maths.lth.se/help/R/ExceptionHandlingInR/
+        tryCatch({
+            data <- x$get()
+            m <- solve(data,...)
+            x$setinverse(m)
+            return(m)
+        }, error = function(ex) {
+            cat("An error was detected.\n")
+            print(ex)
+        }) # tryCatch()
+    } # else !is.null
 }
